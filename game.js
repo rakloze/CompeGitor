86b570eed9e58ba4b42593a2fa80137df2f5b2bc
TheMusician <82782716+rakloze@users.noreply.github.com>
2024-06-20 02:02:25 +0300

    Update game.js
    added modes, need to add levels because modes are though
    
    Signed-off-by: TheMusician <82782716+rakloze@users.noreply.github.com>

--- game.js
+++ game.js
@@ -203,80 +203,185 @@ const scalePositions = {
         { string: '4', fret: '9' }, { string: '4', fret: '11' }, 
         { string: '5', fret: '9' }, { string: '5', fret: '11' }, { string: '5', fret: '12' },
         { string: '6', fret: '9' }, { string: '6', fret: '11' }, { string: '6', fret: '12' }
-    ], 
-    'A Major': [
-        { string: '1', fret: '4' }, { string: '1', fret: '5' },
-        { string: '2', fret: '5' }, { string: '2', fret: '7' },
-        { string: '3', fret: '4' }, { string: '3', fret: '6' }, { string: '3', fret: '7' },
-        { string: '4', fret: '4' }, { string: '4', fret: '6' }, { string: '4', fret: '7' },
-        { string: '5', fret: '4' }, { string: '5', fret: '5' }, { string: '5', fret: '7' },
-        { string: '6', fret: '5' }, { string: '6', fret: '7' }
     ],
-    'G#/Ab Major': [
-        { string: '1', fret: '3' }, { string: '1', fret: '4' },
-        { string: '2', fret: '4' }, { string: '2', fret: '6' },
-        { string: '3', fret: '3' }, { string: '3', fret: '5' }, { string: '3', fret: '6' },
-        { string: '4', fret: '3' }, { string: '4', fret: '5' }, { string: '4', fret: '6' },
-        { string: '5', fret: '3' }, { string: '5', fret: '4' }, { string: '5', fret: '6' },
-        { string: '6', fret: '4' }, { string: '6', fret: '6' }
-    ],
-    'G Major': [
-        { string: '1', fret: '2' }, { string: '1', fret: '3' },
-        { string: '2', fret: '3' }, { string: '2', fret: '5' },
-        { string: '3', fret: '2' }, { string: '3', fret: '4' }, { string: '3', fret: '5' },
-        { string: '4', fret: '2' }, { string: '4', fret: '4' }, { string: '4', fret: '5' },
-        { string: '5', fret: '2' }, { string: '5', fret: '3' }, { string: '5', fret: '5' },
-        { string: '6', fret: '3' }, { string: '6', fret: '5' }
-    ],
-    'F#/Gb Major': [
-        { string: '1', fret: '1' }, { string: '1', fret: '2' },
-        { string: '2', fret: '2' }, { string: '2', fret: '4' },
-        { string: '3', fret: '1' }, { string: '3', fret: '3' }, { string: '3', fret: '4' },
-        { string: '4', fret: '1' }, { string: '4', fret: '3' }, { string: '4', fret: '4' },
-        { string: '5', fret: '1' }, { string: '5', fret: '2' }, { string: '5', fret: '4' },
-        { string: '6', fret: '2' }, { string: '6', fret: '4' }
-    ],
-    'A#/Bb Major': [
-        { string: '1', fret: '5' }, { string: '1', fret: '6' },
-        { string: '2', fret: '6' }, { string: '2', fret: '8' },
-        { string: '3', fret: '5' }, { string: '3', fret: '7' }, { string: '3', fret: '8' },
-        { string: '4', fret: '5' }, { string: '4', fret: '7' }, { string: '4', fret: '8' },
-        { string: '5', fret: '5' }, { string: '5', fret: '6' }, { string: '5', fret: '8' },
-        { string: '6', fret: '6' }, { string: '6', fret: '8' }
-    ],
-    'B Major': [
-        { string: '1', fret: '6' }, { string: '1', fret: '7' },
-        { string: '2', fret: '7' }, { string: '2', fret: '9' },
-        { string: '3', fret: '6' }, { string: '3', fret: '8' }, { string: '3', fret: '9' },
-        { string: '4', fret: '6' }, { string: '4', fret: '8' }, { string: '4', fret: '9' },
-        { string: '5', fret: '6' }, { string: '5', fret: '7' }, { string: '5', fret: '9' },
-        { string: '6', fret: '7' }, { string: '6', fret: '9' }
-    ],
-    'C Major': [
-        { string: '1', fret: '7' }, { string: '1', fret: '8' },
-        { string: '2', fret: '8' }, { string: '2', fret: '10' },
-        { string: '3', fret: '7' }, { string: '3', fret: '9' }, { string: '3', fret: '10' },
-        { string: '4', fret: '7' }, { string: '4', fret: '9' }, { string: '4', fret: '10' },
+
+// C Modes
+    'C Ionian': [
+        { string: '6', fret: '8' }, { string: '6', fret: '10' },
         { string: '5', fret: '7' }, { string: '5', fret: '8' }, { string: '5', fret: '10' },
-        { string: '6', fret: '8' }, { string: '6', fret: '10' }
+        { string: '4', fret: '7' }, { string: '4', fret: '9' }, { string: '4', fret: '10' },
+        { string: '3', fret: '7' }, { string: '3', fret: '9' }, { string: '3', fret: '10' },
+        { string: '2', fret: '8' }, { string: '2', fret: '10' },
+        { string: '1', fret: '7' }, { string: '1', fret: '8' }
     ],
-    'C#/Db Major': [
-        { string: '1', fret: '8' }, { string: '1', fret: '9' },
-        { string: '2', fret: '9' }, { string: '2', fret: '11' },
-        { string: '3', fret: '8' }, { string: '3', fret: '10' }, { string: '3', fret: '11' },
-        { string: '4', fret: '8' }, { string: '4', fret: '10' }, { string: '4', fret: '11' },
-        { string: '5', fret: '8' }, { string: '5', fret: '9' }, { string: '5', fret: '11' },
-        { string: '6', fret: '9' }, { string: '6', fret: '11' }
+    'C Dorian': [
+        { string: '6', fret: '8' }, { string: '6', fret: '10' }, { string: '6', fret: '11' },
+        { string: '5', fret: '8' }, { string: '5', fret: '10' }, { string: '5', fret: '12' },
+        { string: '4', fret: '7' }, { string: '4', fret: '10' },
+        { string: '3', fret: '7' }, { string: '3', fret: '8' }, { string: '3', fret: '10' },
+        { string: '2', fret: '8' }, { string: '2', fret: '10' }, { string: '2', fret: '11' },
+        { string: '1', fret: '8' }, { string: '1', fret: '10' }
     ],
-    'D Major': [
-        { string: '1', fret: '9' }, { string: '1', fret: '10' },
-        { string: '2', fret: '10' }, { string: '2', fret: '12' },
-        { string: '3', fret: '9' }, { string: '3', fret: '11' }, { string: '3', fret: '12' },
-        { string: '4', fret: '9' }, { string: '4', fret: '11' }, { string: '4', fret: '12' },
+    'C Phrygian': [
+        { string: '6', fret: '8' }, { string: '6', fret: '9' }, { string: '6', fret: '11' },
+        { string: '5', fret: '8' }, { string: '5', fret: '10' }, { string: '5', fret: '11' },
+        { string: '4', fret: '8' }, { string: '4', fret: '10' },
+        { string: '3', fret: '7' }, { string: '3', fret: '8' }, { string: '3', fret: '10' },
+        { string: '2', fret: '8' }, { string: '2', fret: '9' }, { string: '2', fret: '11' },
+        { string: '1', fret: '8' }
+    ],
+    'C Lydian': [
+        { string: '6', fret: '8' }, { string: '6', fret: '10' },
+        { string: '5', fret: '7' }, { string: '5', fret: '9' }, { string: '5', fret: '10' },
+        { string: '4', fret: '7' }, { string: '4', fret: '9' }, { string: '4', fret: '10' },
+        { string: '3', fret: '7' }, { string: '3', fret: '9' }, 
+        { string: '2', fret: '7' }, { string: '2', fret: '8' }, { string: '2', fret: '10' },
+        { string: '1', fret: '7' }, { string: '1', fret: '8' }
+    ],
+    'C Mixolydian': [
+        { string: '6', fret: '8' }, { string: '6', fret: '10' }, 
+        { string: '5', fret: '7' }, { string: '5', fret: '8' }, { string: '5', fret: '10' },
+        { string: '4', fret: '7' }, { string: '4', fret: '8' }, { string: '4', fret: '10' },
+        { string: '3', fret: '7' }, { string: '3', fret: '9' }, { string: '3', fret: '10' },
+        { string: '2', fret: '8' }, { string: '2', fret: '10' }, { string: '2', fret: '11' },
+        { string: '1', fret: '8' }
+    ],
+    'C Aeolian': [
+        { string: '6', fret: '8' }, { string: '6', fret: '10' }, { string: '6', fret: '11' },
+        { string: '5', fret: '8' }, { string: '5', fret: '10' }, { string: '5', fret: '11' },
+        { string: '4', fret: '8' }, { string: '4', fret: '10' },
+        { string: '3', fret: '7' }, { string: '3', fret: '8' }, { string: '3', fret: '10' },
+        { string: '2', fret: '8' }, { string: '2', fret: '9' }, { string: '2', fret: '11' },
+        { string: '1', fret: '8' }
+    ],
+    'C Locrian': [
+        { string: '6', fret: '8' }, { string: '6', fret: '9' }, { string: '6', fret: '11' },
+        { string: '5', fret: '8' }, { string: '5', fret: '10' }, { string: '5', fret: '11' },
+        { string: '4', fret: '8' }, { string: '4', fret: '10' },
+        { string: '3', fret: '7' }, { string: '3', fret: '8' }, { string: '3', fret: '10' },
+        { string: '2', fret: '8' }, { string: '2', fret: '9' }, { string: '2', fret: '11' },
+        { string: '1', fret: '8' }
+    ],
+
+// D Modes
+    'D Ionian': [
+        { string: '6', fret: '10' }, { string: '6', fret: '12' },
         { string: '5', fret: '9' }, { string: '5', fret: '10' }, { string: '5', fret: '12' },
-        { string: '6', fret: '10' }, { string: '6', fret: '12' }
+        { string: '4', fret: '9' }, { string: '4', fret: '11' }, { string: '4', fret: '12' },
+        { string: '3', fret: '9' }, { string: '3', fret: '11' }, { string: '3', fret: '12' },
+        { string: '2', fret: '10' }, { string: '2', fret: '12' },
+        { string: '1', fret: '9' }, { string: '1', fret: '10' }
     ],
+    'D Dorian': [
+        { string: '6', fret: '10' }, { string: '6', fret: '12' }, { string: '6', fret: '13' },
+        { string: '5', fret: '10' }, { string: '5', fret: '12' }, { string: '5', fret: '14' },
+        { string: '4', fret: '9' }, { string: '4', fret: '12' },
+        { string: '3', fret: '9' }, { string: '3', fret: '10' }, { string: '3', fret: '12' },
+        { string: '2', fret: '10' }, { string: '2', fret: '12' }, { string: '2', fret: '13' },
+        { string: '1', fret: '10' }, { string: '1', fret: '12' }
+    ],
+    'D Phrygian': [
+        { string: '6', fret: '10' }, { string: '6', fret: '11' }, { string: '6', fret: '13' },
+        { string: '5', fret: '10' }, { string: '5', fret: '12' }, { string: '5', fret: '13' },
+        { string: '4', fret: '10' }, { string: '4', fret: '12' }, { string: '4', fret: '13' },
+        { string: '3', fret: '10' }, { string: '3', fret: '12' },
+        { string: '2', fret: '10' }, { string: '2', fret: '11' }, { string: '2', fret: '13' },
+        { string: '1', fret: '10' }, { string: '1', fret: '11' }, { string: '1', fret: '13' }
+    ],
+    'D Lydian': [
+        { string: '6', fret: '10' }, { string: '6', fret: '12' }, 
+        { string: '5', fret: '9' }, { string: '5', fret: '11' }, { string: '5', fret: '12' },
+        { string: '4', fret: '9' }, { string: '4', fret: '11' }, { string: '4', fret: '12' },
+        { string: '3', fret: '9' }, { string: '3', fret: '11' },
+        { string: '2', fret: '10' }, { string: '2', fret: '12' },
+        { string: '1', fret: '9' }, { string: '1', fret: '11' }
+    ],
+    'D Mixolydian': [
+        { string: '6', fret: '10' }, { string: '6', fret: '12' }, 
+        { string: '5', fret: '9' }, { string: '5', fret: '10' }, { string: '5', fret: '12' },
+        { string: '4', fret: '9' }, { string: '4', fret: '10' }, { string: '4', fret: '12' },
+        { string: '3', fret: '9' }, { string: '3', fret: '11' }, { string: '3', fret: '12' },
+        { string: '2', fret: '10' }, { string: '2', fret: '12' }, { string: '2', fret: '13' },
+        { string: '1', fret: '10' }
+    ],
+    'D Aeolian': [
+        { string: '6', fret: '10' }, { string: '6', fret: '12' }, { string: '6', fret: '13' },
+        { string: '5', fret: '10' }, { string: '5', fret: '12' }, { string: '5', fret: '13' },
+        { string: '4', fret: '10' }, { string: '4', fret: '12' },
+        { string: '3', fret: '10' }, { string: '3', fret: '12' },
+        { string: '2', fret: '10' }, { string: '2', fret: '11' }, { string: '2', fret: '13' },
+        { string: '1', fret: '10' }
+    ],
+    'D Locrian': [
+        { string: '6', fret: '10' }, { string: '6', fret: '11' }, { string: '6', fret: '13' },
+        { string: '5', fret: '10' }, { string: '5', fret: '12' }, { string: '5', fret: '13' },
+        { string: '4', fret: '10' }, { string: '4', fret: '12' }, { string: '4', fret: '13' },
+        { string: '3', fret: '10' }, { string: '3', fret: '12' },
+        { string: '2', fret: '10' }, { string: '2', fret: '11' }, { string: '2', fret: '13' },
+        { string: '1', fret: '10' }
+    ],
+
+// A Modes
+    'A Ionian': [
+        { string: '6', fret: '5' }, { string: '6', fret: '7' },
+        { string: '5', fret: '4' }, { string: '5', fret: '5' }, { string: '5', fret: '7' },
+        { string: '4', fret: '4' }, { string: '4', fret: '6' }, { string: '4', fret: '7' },
+        { string: '3', fret: '4' }, { string: '3', fret: '6' }, { string: '3', fret: '7' },
+        { string: '2', fret: '5' }, { string: '2', fret: '7' },
+        { string: '1', fret: '4' }, { string: '1', fret: '5' }
+    ],
+    'A Dorian': [
+        { string: '6', fret: '5' }, { string: '6', fret: '7' }, { string: '6', fret: '8' },
+        { string: '5', fret: '5' }, { string: '5', fret: '7' }, { string: '5', fret: '9' },
+        { string: '4', fret: '4' }, { string: '4', fret: '7' },
+        { string: '3', fret: '4' }, { string: '3', fret: '5' }, { string: '3', fret: '7' },
+        { string: '2', fret: '5' }, { string: '2', fret: '7' }, { string: '2', fret: '8' },
+        { string: '1', fret: '5' }, { string: '1', fret: '7' }
+    ],
+    'A Phrygian': [
+        { string: '6', fret: '5' }, { string: '6', fret: '6' }, { string: '6', fret: '8' },
+        { string: '5', fret: '5' }, { string: '5', fret: '7' }, { string: '5', fret: '8' },
+        { string: '4', fret: '5' }, { string: '4', fret: '7' },
+        { string: '3', fret: '4' }, { string: '3', fret: '5' }, { string: '3', fret: '7' },
+        { string: '2', fret: '5' }, { string: '2', fret: '6' }, { string: '2', fret: '8' },
+        { string: '1', fret: '5' }
+    ],
+    'A Lydian': [
+        { string: '6', fret: '5' }, { string: '6', fret: '7' },
+        { string: '5', fret: '4' }, { string: '5', fret: '6' }, { string: '5', fret: '7' },
+        { string: '4', fret: '4' }, { string: '4', fret: '6' }, { string: '4', fret: '8' },
+        { string: '3', fret: '4' }, { string: '3', fret: '6' }, { string: '3', fret: '7' },
+        { string: '2', fret: '5' }, { string: '2', fret: '7' },
+        { string: '1', fret: '6' }, { string: '1', fret: '7' }
+    ],
+    'A Mixolydian': [
+        { string: '6', fret: '5' }, { string: '6', fret: '7' },
+        { string: '5', fret: '4' }, { string: '5', fret: '5' }, { string: '5', fret: '7' },
+        { string: '4', fret: '4' }, { string: '4', fret: '6' }, { string: '4', fret: '7' },
+        { string: '3', fret: '4' }, { string: '3', fret: '6' }, { string: '3', fret: '7' },
+        { string: '2', fret: '5' }, { string: '2', fret: '7' }, { string: '2', fret: '8' },
+        { string: '1', fret: '5' }
+    ],
+    'A Aeolian': [
+        { string: '6', fret: '5' }, { string: '6', fret: '7' }, { string: '6', fret: '8' },
+        { string: '5', fret: '5' }, { string: '5', fret: '7' }, { string: '5', fret: '8' },
+        { string: '4', fret: '5' }, { string: '4', fret: '7' },
+        { string: '3', fret: '4' }, { string: '3', fret: '5' }, { string: '3', fret: '7' },
+        { string: '2', fret: '5' }, { string: '2', fret: '6' }, { string: '2', fret: '8' },
+        { string: '1', fret: '5' }
+    ],
+    'A Locrian': [
+        { string: '6', fret: '5' }, { string: '6', fret: '6' }, { string: '6', fret: '8' },
+        { string: '5', fret: '5' }, { string: '5', fret: '7' }, { string: '5', fret: '8' },
+        { string: '4', fret: '5' }, { string: '4', fret: '7' },
+        { string: '3', fret: '4' }, { string: '3', fret: '5' }, { string: '3', fret: '7' },
+        { string: '2', fret: '5' }, { string: '2', fret: '6' }, { string: '2', fret: '8' },
+        { string: '1', fret: '5' }
+    ]
 };
+
+    
+
 
 function handleFretClick(event) {
     const button = event.target;
